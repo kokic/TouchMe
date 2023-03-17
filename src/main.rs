c660a0230b96bff9a44fd298bc20b97031589fff
kokic <kokicplus@gmail.com>
2023-03-17 09:42:11 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use std::io::{stdin, stdout, Write};
+use std::fs;
+// use std::io::{stdin, stdout, Write};
 
 #[derive(Clone)]
 enum Val {
@@ -8,6 +9,15 @@ enum Val {
     Symbol(String),
     List(Vec<Val>),
     Function(fn(&[Val]) -> Val),
+}
+
+fn vec_val_to_string(vec: Vec<Val>) -> String {
+    let data = vec
+        .iter()
+        .map(|x| x.to_string())
+        .reduce(|s, t| s + &t)
+        .unwrap();
+    "[".to_owned() + &data + "]"
 }
 
 impl std::fmt::Display for Val {
@@ -20,8 +30,8 @@ impl std::fmt::Display for Val {
             match self {
                 Val::Number(n) => n.to_string(),
                 Val::Boolean(b) => b.to_string(),
-                Val::Symbol(sym) => sym.to_string(),
-                Val::List(_) => "#todo".to_string(),
+                Val::Symbol(s) => s.to_string(),
+                Val::List(xs) => vec_val_to_string(xs.to_vec()),
                 Val::Function(_) => "#todo".to_string(),
             }
         )
@@ -46,6 +56,13 @@ fn parse(tokens: &mut Vec<String>) -> Val {
         Val::Boolean(true)
     } else if tok == "false" {
         Val::Boolean(false)
+    } else if tok == "[" {
+        let mut list = Vec::new();
+        while tokens[0] != "]" {
+            list.push(parse(tokens));
+        }
+        tokens.remove(0);
+        return Val::List(list);
     }
     /* else if tok == "#" {
         let bool_token = tokens.remove(0);
@@ -76,7 +93,7 @@ fn eval(expr: &Val, env: &mut HashMap<String, Val>) -> Val {
             let args = &list[1..];
 
             match first {
-                Val::Symbol(sym) => match sym.as_str() {
+                Val::Symbol(symbol) => match symbol.as_str() {
                     "def" => {
                         let name = match &args[0] {
                             Val::Symbol(s) => s.clone(),
@@ -147,15 +164,28 @@ fn main() {
     env.insert("/".to_string(), Val::Function(div));
     env.insert("div".to_string(), Val::Function(div));
 
-    loop {
-        let mut input = String::new();
-        print!(">>> ");
-        stdout().flush().expect("flush failed");
-        stdin().read_line(&mut input).expect("read failed");
-        let mut tokens: Vec<_> = input.split_whitespace().map(|s| s.to_string()).collect();
+    let contents =
+        fs::read_to_string("./main.lsp").expect("should have been able to read the file");
 
-        let expr = parse(&mut tokens);
-        let result = eval(&expr, &mut env);
-        println!("{}", result);
+    for line in contents.lines() {
+        if !line.is_empty() {
+            let mut tokens: Vec<_> = line.split_whitespace().map(|s| s.to_string()).collect();
+            let expr = parse(&mut tokens);
+            let result = eval(&expr, &mut env);
+            println!("{}", result);
+        }
     }
+
+    // loop {
+    //     let mut input = String::new();
+    //     print!(">>> ");
+    //     stdout().flush().expect("flush failed");
+    //     stdin().read_line(&mut input).expect("read failed");
+
+    //     let mut tokens: Vec<_> = input.split_whitespace().map(|s| s.to_string()).collect();
+
+    //     let expr = parse(&mut tokens);
+    //     let result = eval(&expr, &mut env);
+    //     println!("{}", result);
+    // }
 }
