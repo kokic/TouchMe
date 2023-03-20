f78c8d6a5df2091b69eb1e34311ea8eb1406f914
kokic <3250679033@qq.com>
2023-03-20 12:56:47 +0800

    string_owned

--- src/compiler/tokenizer.rs
+++ src/compiler/tokenizer.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 
 
 // enum TouchMeTokenCategory {
@@ -61,7 +60,10 @@ pub fn is_builtin_operator(x: char) -> bool {
         || (x == ')')
 }
 
-pub fn is_identifier(x: char) -> bool {
+
+
+
+pub fn is_identifier_body(x: char) -> bool {
     x.is_alphabetic() || x.is_ascii_digit() || x == '_' || x == '-'
 }
 
@@ -72,18 +74,11 @@ pub fn is_identifier(x: char) -> bool {
 
 
 
-
-
-pub fn token<'a>(
-    expected: &'static str,
-) -> impl Fn(&'a str) -> Result<(&'a str, &'a str), ParseError<'a>> {
-    move |input| match input.starts_with(expected) {
-        true => {
-            let len = expected.len();
-            Ok((&input[len..], &input[..len]))
-        }
-        false => Err(ParseError{ location: input, expected: expected }),
-    }
+#[derive(Debug, PartialEq)]
+pub struct ParseError<'a> {
+    pub location: &'a str, 
+    pub expected: &'static str, 
+    
 }
 
 
@@ -107,225 +102,30 @@ pub fn many<T>(
     }
 }
 
-#[derive(Debug, PartialEq)]
-pub struct ParseError<'a> {
-    pub location: &'a str, 
-    pub expected: &'static str, 
-    
-}
 
 
 
+// pub fn string<'a>(
+//     expected: &'static str,
+// ) -> impl Fn(&'a str) -> Result<(&'a str, &'a str), ParseError<'a>> {
+//     move |input| match input.starts_with(expected) {
+//         true => {
+//             let len = expected.len();
+//             Ok((&input[len..], &input[..len]))
+//         }
+//         false => Err(ParseError{ location: input, expected: expected }),
+//     }
+// }
 
- pub enum Element {
-    String(String),
-    Number(f64),
-    Boolean(bool),
-    Null,
-    Array(Vec<Element>),
-    Object(HashMap<String, Element>),
-}
 
-
-pub struct Parser<'a> {
-    pub input: &'a str,
-}
-
-impl<'a> Parser<'a> {
-
-    pub fn string(&self, pattern: &str) -> Option<(&str, ())> {
-        let mut iter = pattern.chars();
-
-        for p in self.input.chars() {
-            match iter.next() {
-                Some(c) if c == p => {}
-                _ => {
-                    return None;
-                }
-            }
-
-            if iter.as_str().is_empty() {
-                let rest = &self.input[pattern.len()..];
-                return Some((rest, ()));
-            }
+pub fn string_owned<'a>(
+    expected: &'static str,
+) -> impl Fn(&'a str) -> Result<(&'a str, String), ParseError<'a>> {
+    move |input| match input.starts_with(expected) {
+        true => {
+            let len = expected.len();
+            Ok((&input[len..], input[..len].to_owned()))
         }
-        None
+        false => Err(ParseError{ location: input, expected: expected }),
     }
-
-    // fn number(&self) -> Option<(&str, Element)> {
-    //     let mut chars = self.input.chars().peekable();
-    //     let mut num_str = String::new();
-
-    //     while let Some(ch) = chars.peek() {
-    //         if (*ch >= '0' && *ch <= '9') || *ch == '-' || *ch == '+' || *ch == '.' || *ch == 'e' {
-    //             num_str.push(*ch);
-    //         } else {
-    //             break;
-    //         }
-    //         chars.next();
-    //     }
-
-    //     if num_str.is_empty() {
-    //         None
-    //     } else {
-    //         let rest = chars.as_str();
-    //         Some((rest, Element::Number(num_str.parse().unwrap())))
-    //     }
-    // }
-
-    pub fn boolean(&self) -> Option<(&str, Element)> {
-        let pattern_true = "true";
-        let pattern_false = "false";
-        match self.string(pattern_true) {
-            Some((rest, _)) => Some((rest, Element::Boolean(true))),
-            None => match self.string(pattern_false) {
-                Some((rest, _)) => Some((rest, Element::Boolean(false))),
-                None => None,
-            },
-        }
-    }
-
-    pub fn null(&self) -> Option<(&str, Element)> {
-        match self.string("null") {
-            Some((rest, _)) => Some((rest, Element::Null)),
-            None => None,
-        }
-    }
-
-    pub fn comma(&self) -> Option<(&str, ())> {
-        self.string(",").map(|(rest, _)| (rest, ()))
-    }
-
-
-
-
-
-    // fn array(&self) -> Option<(&str, Element)> {
-    //     let mut elements = Vec::new();
-    //     let mut rest = self.input;
-    //     loop {
-    //         match self.element()(rest) {
-    //             Some((r, elem)) => {
-    //                 rest = r.trim_start();
-    //                 elements.push(elem);
-    //             }
-    //             None => break,
-    //         }
-
-    //         match self.comma()(rest) {
-    //             Some((r, _)) => rest = r,
-    //             None => break,
-    //         }
-    //     }
-
-    //     match self.string("]") {
-    //         Some((rest, _)) => Some((rest, Element::Array(elements))),
-    //         None => None,
-    //     }
-    // }
-
-    // fn object_key(&self) -> Option<(&str, String)> {
-    //     let mut chars = self.input.chars().peekable();
-    //     let mut key = String::new();
-
-    //     while let Some(ch) = chars.next() {
-    //         if ch == '"' {
-    //             while let Some(next_ch) = chars.peek() {
-    //                 if *next_ch == '"' {
-    //                     chars.next();
-    //                     return Some((&chars.as_str()[1..], key));
-    //                 }
-    //                 key.push(chars.next().unwrap());
-    //             }
-    //         } else if !ch.is_whitespace() {
-    //             return None;
-    //         }
-    //     }
-
-    //     None
-    // }
-
-    // fn object(&self) -> Option<(&str, Element)> {
-    //     let mut object = HashMap::new();
-    //     let mut rest = self.input;
-    //     loop {
-    //         match self.object_key()(rest) {
-    //             Some((r, key)) => {
-    //                 rest = r.trim_start();
-    //                 if let Some((r, _)) = self.string(":")(rest) {
-    //                     rest = r.trim_start();
-    //                     if let Some((r, value)) = self.element()(rest) {
-    //                         rest = r.trim_start();
-    //                         object.insert(key, value);
-    //                     }
-    //                 }
-    //             }
-    //             None => break,
-    //         }
-
-    //         match self.comma()(rest) {
-    //             Some((r, _)) => rest = r,
-    //             None => break,
-    //         }
-    //     }
-
-    //     match self.string("}") {
-    //         Some((rest, _)) => Some((rest, Element::Object(object))),
-    //         None => None,
-    //     }
-    // }
-    
-    // fn element(&self) -> impl Fn(&str) -> Option<(&str, Element)> {
-    //     move |input: &str| {
-    //         Parser { input }.string("\\\\\"").and_then(|(rest, _)| {
-    //             let mut chars = rest.chars();
-    //             let mut s = String::new();
-
-    //             while let Some(ch) = chars.next() {
-    //                 if ch == '\\' {
-    //                     match chars.next() {
-    //                         Some('"') => s.push('"'),
-    //                         Some('\\') => s.push('\\'),
-    //                         Some('/') => s.push('/'),
-    //                         Some('b') => s.push('\u{0008}'),
-    //                         Some('f') => s.push('\u{000C}'),
-    //                         Some('n') => s.push('\n'),
-    //                         Some('r') => s.push('\r'),
-    //                         Some('t') => s.push('\t'),
-    //                         Some('u') => {
-    //                             let uc: String = chars.by_ref().take(4).collect();
-    //                             match u32::from_str_radix(&uc, 16) {
-    //                                 Ok(cp) => {
-    //                                     match char::from_u32(cp) {
-    //                                         Some(c) => s.push(c),
-    //                                         None => {
-    //                                             return None;
-    //                                         }
-    //                                     };
-    //                                 }
-    //                                 Err(_) => {
-    //                                     return None;
-    //                                 }
-    //                             };
-    //                         }
-    //                         _ => {
-    //                             return None;
-    //                         }
-    //                     }
-    //                 } else if ch == '"' {
-    //                     return Some((&rest[chars.as_str().len()..], Element::String(s)));
-    //                 } else {
-    //                     s.push(ch)
-    //                 }
-    //             }
-
-    //             None
-    //         }).or_else(|| self.number())
-    //           .or_else(|| self.boolean())
-    //           .or_else(|| self.null())
-    //           .or_else(|| self.array())
-    //           .or_else(|| self.object())
-    //     }
-    // }
-    
 }
--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,6 @@ enum Val {
     Function(fn(&[Val]) -> Val),
 }
 
-fn vec_val_to_string(vec: Vec<Val>) -> String {
-    let data = vec
-        .iter()
-        .map(|x| x.to_string())
-        .reduce(|s, t| s + &t)
-        .unwrap();
-    "[".to_owned() + &data + "]"
-}
-
 impl std::fmt::Display for Val {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // write!(f, "#unsupported");
@@ -34,8 +25,8 @@ impl std::fmt::Display for Val {
             match self {
                 Val::Number(n) => n.to_string(),
                 Val::Boolean(b) => b.to_string(),
-                Val::Symbol(s) => s.to_string(),
-                Val::List(xs) => vec_val_to_string(xs.to_vec()),
+                Val::Symbol(sym) => sym.to_string(),
+                Val::List(_) => "#todo".to_string(),
                 Val::Function(_) => "#todo".to_string(),
             }
         )
@@ -60,13 +51,6 @@ fn parse(tokens: &mut Vec<String>) -> Val {
         Val::Boolean(true)
     } else if tok == "false" {
         Val::Boolean(false)
-    } else if tok == "[" {
-        let mut list = Vec::new();
-        while tokens[0] != "]" {
-            list.push(parse(tokens));
-        }
-        tokens.remove(0);
-        return Val::List(list);
     }
     /* else if tok == "#" {
         let bool_token = tokens.remove(0);
@@ -97,7 +81,7 @@ fn eval(expr: &Val, env: &mut HashMap<String, Val>) -> Val {
             let args = &list[1..];
 
             match first {
-                Val::Symbol(symbol) => match symbol.as_str() {
+                Val::Symbol(sym) => match sym.as_str() {
                     "def" => {
                         let name = match &args[0] {
                             Val::Symbol(s) => s.clone(),
@@ -107,11 +91,11 @@ fn eval(expr: &Val, env: &mut HashMap<String, Val>) -> Val {
                         env.insert(name, value);
                         Val::Number(0.0)
                     }
-                    s => match eval(&first, env) {
+                    _ => match eval(&first, env) {
                         Val::Function(f) => {
                             f(&args.iter().map(|arg| eval(arg, env)).collect::<Vec<Val>>())
                         }
-                        v => panic!("Invalid function call for symbol {}: {}", s, v),
+                        _ => panic!("Invalid function call"),
                     },
                 },
                 _ => panic!("Invalid function call"),
@@ -152,11 +136,29 @@ fn div(args: &[Val]) -> Val {
     number_reduce(args, |x, y| x / y)
 }
 
+*/
+
+fn parse_a(input: &str) -> Result<(&str, String), ParseError> {
+    compiler::tokenizer::string_owned("a")(input)
+}
+
+
+
 fn main() {
-    let mut env = HashMap::new();
-    env.insert("+".to_string(), Val::Function(add));
-    env.insert("add".to_string(), Val::Function(add));
-    env.insert("sum".to_string(), Val::Function(add));
+    // let parser = compiler::tokenizer::Parser { input: "abc" };
+    // let letter_a = parser.string("a");
+
+    // let letter_a = compiler::tokenizer::token("a");
+    let f = parse_a;
+
+    let a_many = compiler::tokenizer::many(f);
+
+    println!("{:?}", a_many("aaaabc"))
+
+    // let mut env = HashMap::new();
+    // env.insert("+".to_string(), Val::Function(add));
+    // env.insert("add".to_string(), Val::Function(add));
+    // env.insert("sum".to_string(), Val::Function(add));
 
     // env.insert("-".to_string(), Val::Function(sub));
     // env.insert("sub".to_string(), Val::Function(sub));
@@ -168,15 +170,15 @@ fn main() {
     // env.insert("/".to_string(), Val::Function(div));
     // env.insert("div".to_string(), Val::Function(div));
 
-    loop {
-        let mut input = String::new();
-        print!(">>> ");
-        stdout().flush().expect("flush failed");
-        stdin().read_line(&mut input).expect("read failed");
-        let mut tokens: Vec<_> = input.split_whitespace().map(|s| s.to_string()).collect();
+    // loop {
+    //     let mut input = String::new();
+    //     print!(">>> ");
+    //     stdout().flush().expect("flush failed");
+    //     stdin().read_line(&mut input).expect("read failed");
+    //     let mut tokens: Vec<_> = input.split_whitespace().map(|s| s.to_string()).collect();
 
-        let expr = parse(&mut tokens);
-        let result = eval(&expr, &mut env);
-        println!("{}", result);
-    }
+    //     let expr = parse(&mut tokens);
+    //     let result = eval(&expr, &mut env);
+    //     println!("{}", result);
+    // }
 }
